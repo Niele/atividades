76a81638b48c653ac3c74e0ef43d0751006b31f7
Niele <nieleerica18@hotmail.com>
2014-01-13 17:30:04 -0200

    Create form_cadastrar.php

---
+++ avaliacao3/form_cadastrar.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Cadastro de produto</title>           
+    </head>
+    <body>
+        
+        <?php
+            require_once("menu.php");
+        ?>
+        <form action="cadastrar.php" method="post">
+            <?php require_once("campos_pessoa.php"); ?>
+            <input type="submit" value="Enviar" />
+               
+        </form>
+         
+        
+    </body>
+</html>
