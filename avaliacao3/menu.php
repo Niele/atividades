fc7fde8cbcbf25e0415f6a4ec96e8369430e896c
Niele <nieleerica18@hotmail.com>
2014-01-13 17:32:03 -0200

    Create menu.php

---
+++ avaliacao3/menu.php
@@ -0,0 +1,9 @@
+
+<a href="index.php">Listar de Produto</a>
+&nbsp;&nbsp;
+<a href="form_cadastrar.php">Cadastrar Produto</a>
+&nbsp;&nbsp;
+<a href="form_editar.php">Editar Produto</a>
+&nbsp;&nbsp;
+<a href="form_remover.php">Remover Produto</a>
+<br/><br/>
