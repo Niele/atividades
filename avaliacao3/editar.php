d1b888f0fac40bac5cec7a54d4ffab603db10cf2
Niele <nieleerica18@hotmail.com>
2014-01-13 17:29:26 -0200

    Create editar.php

---
+++ avaliacao3/editar.php
@@ -0,0 +1,33 @@
+Nome do produto
+<br/>
+<input type="text" name="nome" />
+<br/>
+<br/>
+
+Cor do produto:
+<br/>
+<input type="radio" name="sexo" value="Verde" checked="checked" /> 
+<label>Verde</label>
+<input type="radio" name="sexo" value="Rosa"/>
+Rosa
+<input type="radio" name="sexo" value="Amarelo"/>
+Amarelo
+<br/>
+<br/>
+<input type="checkbox" name="aceito" value="Sim" />
+
+Produto novo:
+<br/>
+<br/>
+
+Opções de entrega:
+<select name="estado">
+    <option>5 dias</option>
+    <option>10 dias</option>
+    <option>15 dias</option>
+</select>
+<br/>
+
+Detalhes do Produto:<br/>
+<textarea name="observacoes" MAXLENGTH="180"></textarea>
+<br/>
